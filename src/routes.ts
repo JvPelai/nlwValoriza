4429bd47e34b3af36aa4caa18ce94390cfd94bec
JvPelai <jv.pelai@gmail.com>
2021-06-25 20:14:09 -0300

    ensureAdmin middleware

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
+import { RepositoryNotFoundError } from "typeorm";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateUserController } from "./controllers/CreateUserController";
+import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 const router = Router();
 
@@ -8,6 +10,7 @@ const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 
 router.post("/users", createUserController.handle);
-router.post("/tags", createTagController.handle);
+
+router.post("/tags", ensureAdmin, createTagController.handle);
 
 export { router }
